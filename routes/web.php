d49ae31aec0d009b60c827a6973158a4e3d10334
leilaakbari1996 <leila.akbari1996@gmail.com>
2021-09-11 11:52:17 +0430

    add template to project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\admin\HomeController as AdminHomeController;
+use App\Http\Controllers\client\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('client.index');
+Route::get('/adminpanel',[AdminHomeController::class,'index'])->name('admin.index');
