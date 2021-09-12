a81de4bcdaa851da84bd29b306a2f71634e5d578
leilaakbari1996 <leila.akbari1996@gmail.com>
2021-09-12 18:59:42 +0430

    brand client

--- app/Http/Controllers/client/HomeController.php
+++ app/Http/Controllers/client/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('client.home');
+        return view('client.home',[
+            'brands' => Brand::all()
+        ]);
     }
 }
